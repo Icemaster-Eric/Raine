1ae07991d692722bd4e859cef8ff8af620f2c674
Eric Li <icemastereric@gmail.com>
2024-08-14 22:28:47 -0600

    still working on vts stuff

--- vts/vts.py
+++ vts/vts.py
@@ -1,12 +1,17 @@
+from typing import Literal
 from contextlib import asynccontextmanager
+import asyncio
 import pyvts
 
 
 class VTS:
     def __init__(self):
+        self.connected: bool = False
         self.vts = pyvts.vts()
         self.hotkeys: list = [] # not initialized yet
-        self.parameters: list = [] # (default params) not initialized yet
+        self.parameters: dict[str, dict[
+            Literal["value", "min", "max", "defaultValue"], float
+        ]] = {} # (model is constantly set to these values) not initialized yet
 
     async def connect(self):
         await self.vts.connect()
@@ -20,6 +25,19 @@ class VTS:
         self.parameters = parameter_data["data"]["defaultParameters"]
 
         await self.vts.close()
+
+        self.connected = True
+
+        asyncio.create_task(self.send_parameters())
+
+    async def disconnect(self):
+        if self.vts.get_connection_status():
+            self.vts.close()
+
+        self.hotkeys: list = []
+        self.parameters = {}
+
+        self.connected = False
 
     @asynccontextmanager
     async def vts_request(self):
@@ -43,7 +61,7 @@ class VTS:
         Args:
             parameter (str): Name of the parameter.
             value (float): Value of the data, [-1000000, 1000000]
-            weight (float, optional): You can mix the your value with vts face tracking parameter, from 0 to 1,. Defaults to 1.
+            weight (float, optional): You can mix the your value with vts face tracking parameter, from 0 to 1. Defaults to 1.
             face_found (bool, optional): if true, you will tell VTubeStudio to consider the user face as found, s.t. you can control when the "tracking lost". Defaults to False.
             mode (str, optional): Defaults to "set".
 
@@ -59,14 +77,14 @@ class VTS:
                 mode
             )
         )
-    
-    async def set_parameter_values(self, parameters: list[str], values: list[float], weight: float = 1, face_found: bool = False, mode: str = "set") -> dict:
+
+    async def set_parameter_values(self, parameters: list[str], values: list[float], weights: list[float] | float = 1, face_found: bool = False, mode: str = "set") -> dict:
         """Set a parameter to a specific value.
 
         Args:
             parameters (list[str]): Name of the parameter.
             values (list[float]): Value of the data, [-1000000, 1000000]
-            weight (float, optional): You can mix the your value with vts face tracking parameter, from 0 to 1,. Defaults to 1.
+            weight (list[float] | float, optional): You can mix the your value with vts face tracking parameter, from 0 to 1. Defaults to 1.
             face_found (bool, optional): if true, you will tell VTubeStudio to consider the user face as found, s.t. you can control when the "tracking lost". Defaults to False.
             mode (str, optional): Defaults to "set".
 
@@ -74,8 +92,28 @@ class VTS:
             dict: Vtube Studio API Response
         """
         return await self.vts.request(
-            # checking the code and stuff
+            self.vts.vts_request.requestSetMultiParameterValue(
+                parameters,
+                values,
+                weights,
+                face_found,
+                mode
+            )
         )
+
+    async def send_parameters(self):
+        """Constantly sends requests to vtube studio to keep the model at the current parameters.
+
+        Stops when the connected property is set to False.
+        """
+        while self.connected:
+            async with self.vts_request():
+                await self.set_parameter_values(
+                    [param_name for param_name in self.parameters.keys()],
+                    [param["value"] for param in self.parameters.values()],
+                )
+
+            await asyncio.sleep(0.5)
 
     async def trigger(self, hotkey: int | dict) -> dict:
         """Triggers a hotkey.
