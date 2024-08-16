66b49b0644222eeef608822a5fee65dc8d43ee5d
Eric Li <icemastereric@gmail.com>
2024-08-16 15:09:58 -0600

    trying to get tts working (it works?!?!?!?!)

--- tts/tts.py
+++ tts/tts.py
@@ -1,52 +1,48 @@
+from io import BytesIO
+import requests
+from tqdm import tqdm
+from soundfile import read as sf_read
+from sounddevice import play, wait
+
+
 class TTS:
     def __init__(
             self,
-            gpt_path: str,
-            sovits_path: str,
             emotions: dict[str, tuple[str, str]],
-            config_path: str = "gpt_sovits/GPT_SoVITS/configs/tts_infer.yaml",
     ):
-        pass
+        self.emotions = emotions
+        self.emotion = list(emotions.keys())[0]
+        self.set_emotion(self.emotion)
 
     def set_emotion(self, emotion: str):
-        self.emotion = self.emotions[emotion]
+        self.emotion = emotion
+
+        requests.get("http://127.0.0.1:9880/change_refer", params={
+            "refer_wav_path": self.emotions[emotion][0],
+            "prompt_text": self.emotions[emotion][1],
+            "prompt_language": "en"
+        })
 
     def infer(
             self,
             text: str,
             emotion: str | None = None,
-            top_k: int = 5,
-            top_p: float = 1,
-            temp: float = 0.75,
-            rep_pen: float = 1.02,
     ):
         if emotion:
-            if self.emotions[emotion] != self.emotion:
-                self.emotion = self.emotions[emotion]
+            if emotion != self.emotion:
+                self.set_emotion(emotion)
 
-        ref_audio_path, ref_text = self.emotion
-
-        req = {
+        response = requests.get("http://127.0.0.1:9880/", params={
             "text": text,
-            "text_lang": "en",
-            "ref_audio_path": ref_audio_path,
-            "prompt_text": ref_text,
-            "prompt_lang": "en",
-            "top_k": top_k,
-            "top_p": top_p,
-            "temperature": temp,
-            "text_split_method": "cut5",  # str. text split method, see text_segmentation_method.py for details.
-            "batch_size": 4,
-            "batch_threshold": 1,  # float. threshold for batch splitting.
-            "split_bucket": False,  # bool. whether to split the batch into multiple buckets.
-            "speed_factor": 1.0,  # float. control the speed of the synthesized audio.
-            "fragment_interval": 0.3,  # float. to control the interval of the audio fragment.
-            "return_fragment": True,
-            "seed": -1,  # int. random seed for reproducibility.
-            "media_type": "raw",  # str. media type of the output audio, support "wav", "raw", "ogg", "aac".
-            "streaming_mode": True,  # bool. whether to return a streaming response.
-            "parallel_infer": True,  # bool.(optional) whether to use parallel inference.
-            "repetition_penalty": rep_pen
-        }
+            "text_language": "en"
+        }, stream=True)
 
-        # api call here
+        for i, chunk in tqdm(enumerate(response.iter_content(chunk_size=5292000)), "GPT-SoVITS"): # chunk size is *supposed* to be 30 seconds of wav file data
+            if i % 2:
+                #audio_data = 
+                play(*sf_read(BytesIO(chunk)))
+                wait()
+            else:
+                text = chunk.decode("utf-8")
+
+        yield "hi", "hello"
--- tts_model_test.py
+++ tts_model_test.py
@@ -5,18 +5,16 @@ from tts import TTS
 
 async def main():
     tts = TTS(
-        "gpt_sovits/GPT_weights/Raine GPT-SoVITS v1_e15_s165.pth",
-        "gpt_sovits/SoVITS_weights/Raine GPT-SoVITS v1-e18.ckpt",
         {
             "smug": (
-                "tts/ref_audio/smug.wav",
+                "E:/Code/Raine/tts/ref_audio/smug.wav",
                 "Free from social mores and other people's opinions, and no family obligations. Hmph, must be an easy life."
             ),
         }
     )
 
-    for chunk in tts.infer("ok so now I can start working on syncing audio output from the tts to the mouthOpen parameter"):
-        tts.play(chunk)
+    for audio, text in tts.infer("ok so now I can start working on syncing audio output from the tts to the mouthOpen parameter"):
+        pass
 
     return
 
